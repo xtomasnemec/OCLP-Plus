3912e7d69905bfc9afae407089d5036904b026e8
YBronst <157227982+YBronst@users.noreply.github.com>
2026-03-11 22:10:55 +0200

    Update constants.py

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -13,7 +13,7 @@ from .detections import device_probe
 class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
-        self.patcher_version:                 str = "3.1.6"  # OpenCore-Legacy-Patcher
+        self.patcher_version:                 str = "3.1.7"  # OpenCore-Legacy-Patcher
         self.patcher_support_pkg_version:     str = "2.0.0"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2025 Dortania"
         self.patcher_name:                    str = "OpenCore Legacy Patcher"
